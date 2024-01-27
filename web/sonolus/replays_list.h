53d9115ea838ef1379c95f8339cac627bd16ffcd
littleyang0531 <littleyang0531@outlook.com>
2024-01-27 23:03:04 +0800

    Update Sonolus v0.8.0

---
+++ web/sonolus/replays_list.h
@@ -0,0 +1,12 @@
+using namespace std;
+
+auto sonolus_replays_list = [](client_conn conn, http_request request, param argv){
+    auto $_GET = getParam(request);
+    int page = atoi($_GET["page"].c_str());
+    Section<ReplayItem> res = replayList(replayFilter($_GET), page * 20 + 1, (page + 1) * 20);
+    Json::Value val = res.toJsonObject();
+    __api_default_response["Content-Length"] = to_string(json_encode(val).size());
+    putRequest(conn, 200, __api_default_response);
+    send(conn, json_encode(val));
+    exitRequest(conn);
+};
