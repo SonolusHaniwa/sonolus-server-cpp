97672984723728f5eb7a03ac9c4c677959f79c14
littleyang0531 <littleyang0531@outlook.com>
2024-05-05 17:20:43 +0800

    Update libsonolush

--- modules/zip.h
+++ modules/zip.h
@@ -28,3 +28,19 @@ string getFileFromZip(string zipPath, string filePath) {
 	exit(3);
     #endif
 }
+
+vector<string> getFileListFromZip(string zipPath) {
+    #ifdef ENABLE_ZIP
+    zip* z = zip_open(zipPath.c_str(), 0, NULL);
+    if (z == NULL) {
+        writeLog(LOG_LEVEL_ERROR, "Cannot open zip file.");
+        return {};
+    } vector<string> ret;
+    zip_int64_t i, c = zip_get_num_entries(z, ZIP_FL_UNCHANGED);
+    for (i = 0; i < c; i++) ret.push_back(zip_get_name(z, i, ZIP_FL_ENC_GUESS));
+    return ret;
+    #else
+    writeLog(LOG_LEVEL_ERROR, "This program doesn't support libzip. You need to compile this program with the option `-DENABLE_ZIP -lzip`.");
+	exit(3);
+    #endif
+}
